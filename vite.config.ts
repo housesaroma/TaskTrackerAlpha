1e8685507d12a67249a00fbf94e8f21fbda6a468
housesaroma <yahakulikov@yandex.ru>
2025-04-03 20:34:25 +0500

    basic settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import svg from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svg(), // для импорта SVG как React-компонентов
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // алиас для импортов
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+        },
+      },
+    },
+  },
+});
